906c6b408885ca4c0eaa264e8355245e0c3902c4
Praveen <praveennerthethil96@gmail.com>
2021-09-06 12:37:56 +0530

    list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ToDoForm from "./Components/toDoForm";
+import ToDoList from "./Components/ToDoList";
 
 
 function App() {
   return (
     <div className="toDoApp">
-       <ToDoForm/>
+       <ToDoList/>
     </div>
   );
 }
--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react';
+import ToDoForm from './toDoForm';
 
-function toDoList() {
+function ToDoList() {
+    const [todos,setTodo]=useState([])
+    const addToDo=todo=>{
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return;
+        }
+        const newTodos = [todo, ...todos];
+        setTodo(newTodos)
+        console.log(todo, ...todos)
+    } 
+
     return (
         <div>
-            
+            <h1>What's the plan today..</h1>
+            <ToDoForm onSubmit={addToDo}/>
         </div>
     )
 }
 
-export default toDoList
+export default ToDoList
