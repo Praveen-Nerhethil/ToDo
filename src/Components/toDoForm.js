c7e46c1d409ff452b572b6732548910c2d247170
Praveen <praveennerthethil96@gmail.com>
2021-09-06 11:34:04 +0530

    form

--- src/Components/toDoForm.js
+++ src/Components/toDoForm.js
@@ -1,12 +1,25 @@
 import React,{useState} from 'react'
 
-function ToDoForm() {
+function ToDoForm(props) {
     const [input,setInput]=useState('')
 
+    const handleChange=e=>{
+        setInput(e.target.value)
+    }
+
+    const handleSubmit=e=>{
+        e.preventDefault();
+    
+    // props.onSubmit({
+    //     id: Math.floor(Math.random()*1000),
+    //     text: input
+    // });
+        setInput('');
+    };
     return (
         <div>
-        <form className='todo-form'>
-            <input type='text' value={input} name='text' className="todo-input" placeholder="type something..." />
+        <form className='todo-form' onSubmit={handleSubmit}>
+            <input type='text' value={input} name='text' onChange={handleChange} className="todo-input" placeholder="type something..." />
             <button className='todo-button'>
             Add ToDos
         </button>
